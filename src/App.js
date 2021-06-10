66410f2c1074400e64dc1d3259c876f3e5c29482
jokagitani <76771962+jokagitani@users.noreply.github.com>
2021-06-11 01:07:13 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
+import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      <h2>
-        BUABIE
-      </h2>
+      <Header>
+      </Header>
     </div>
   );
 } 
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+const Header = props => {
+    return(
+        <header className="header">
+            <h1 className="h2">
+                Rosy Hands
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
